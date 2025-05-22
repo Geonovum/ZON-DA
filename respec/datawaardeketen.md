b297e261169326c79de49ee620ed77d660ef049b
Niels Hoffmann <n.hoffmann@geonovum.nl>
2025-05-22 09:32:35 +0200

    initieel

---
+++ respec/datawaardeketen.md
@@ -0,0 +1,1 @@
+# Business en proces: de Datawaardeketen ZoN 
---
+++ respec/js/document_config.js
@@ -0,0 +1,38 @@
+var documentConfig =
+{
+  title: "ZON-DA",
+  subtitle: "Zicht op Nederland Doelarchitectuur",
+  // shortName: "NLDT",
+  // pubDomain: "geen",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by",
+  maxTocLevel: 3,
+  // latestVersion: [
+  //   "https://docs.geostandaarden.nl/NLDT-Architectuur/"
+  // ],
+  publishDate: "2025-05-22",
+  publishVersion: "0.1.0",
+  thisVersion: [
+    "https://geonovum.github.io/ZON-DA/"
+  ],
+  github: "https://github.com/geonovum/ZON-DA/",
+  issueBase: "https://github.com/geonovum/ZON-DA/issues/",
+  edDraftURI: "https://geonovum.github.io/ZON-DA/",
+  authors: [
+    
+  ],
+  editors: [
+   
+  ],
+
+  localBiblio: {
+        
+  
+  FDS: {
+    title: "Scope van FDS",
+    href: "https://federatief.datastelsel.nl/kennisbank/scope-van-fds/",
+    date: "30-04-2025"
+  }
+  }
+}
